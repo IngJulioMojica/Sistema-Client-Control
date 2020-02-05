f4523d96507d494d61822c3142380e6ec06f6968
IngJulioMojica <60707174+IngJulioMojica@users.noreply.github.com>
2020-02-05 12:18:39 -0500

    Add files via upload

---
+++ Controllers/DatosController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Client_Control_001.Models;
+
+
+namespace Client_Control_001.Controllers
+{
+    public class DatosController : Controller
+    {
+        //
+        // GET: /Datos/
+
+        Data_Client_ControlEntities entidad = new Data_Client_ControlEntities();
+
+        public ActionResult Index()
+        {
+            var listardatos = entidad.Clientes;
+            return View(listardatos.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult buscarCliente(int id)
+        {
+
+            var modelo = from p in entidad.Clientes where p.Cod_cli == id 
+                         join c in entidad.Ciudades
+                          on p.Cod_ciudad equals c.Cod_ciudad
+                           join o in entidad.Compras
+                            on p.Cod_compra equals o.Cod_compra
+                             join e in entidad.Empresas
+                               on o.Cod_empresa equals e.Cod_empresa
+                                join t in entidad.Empresa_detalle
+                                  on o.Cod_empresa equals t.Cod_empresa
+
+                         select new ElCliente
+                         {
+                            codigo = p.Cod_cli,
+                            nombres = p.Nombres, 
+                            apellidos  = p.Apellidos,
+                            ciudad = c.Nombre_ciudad,
+                            empresa = t.Nombre_empresa
+                         };
+
+            return View(modelo.ToList());
+        
+        }
+
+
+        public ActionResult Listar() 
+        {
+            var modelo2 = from m in entidad.Compras orderby m.Fecha_compra descending 
+                          join p in entidad.Clientes
+                            on m.Cod_cli equals p.Cod_cli
+                             join c in entidad.Compras
+                               on p.Cod_compra equals c.Cod_compra
+                                join e in entidad.Empresas
+                                  on c.Cod_empresa equals e.Cod_empresa
+                                   join d in entidad.Empresa_detalle
+                                     on e.Cod_empresa equals d.Cod_empresa
+
+                          select new LaCompra
+                          {
+                           apellidos = p.Apellidos,
+                           nombres = p.Nombres,
+                           codigocompra = c.Cod_compra,
+                           empresa = e.Cod_empresa,
+                           nombempresa = d.Nombre_empresa,
+                           fechacompra = c.Fecha_compra
+
+                          };
+
+
+            return View(modelo2.ToList());
+        
+        }
+
+
+
+        public ActionResult ActualizarCompras(string nombre) 
+        {
+
+            var modelo3 = from m in entidad.Compras
+                          join p in entidad.Clientes
+                            on m.Cod_cli equals p.Cod_cli
+                          join c in entidad.Compras
+                            on p.Cod_compra equals c.Cod_compra
+                          join e in entidad.Empresas
+                            on c.Cod_empresa equals e.Cod_empresa
+                          join d in entidad.Empresa_detalle
+                            on e.Cod_empresa equals d.Cod_empresa
+                       
+
+                          select new LaCompra
+                          {
+                              apellidos = p.Apellidos,
+                              nombres = p.Nombres,
+                              codigocompra = c.Cod_compra,
+                              empresa = e.Cod_empresa,
+                              nombempresa = d.Nombre_empresa,
+                              fechacompra = c.Fecha_compra
+
+                          };
+
+
+            return View(modelo3.ToList());
+        
+        }
+
+
+        public ActionResult Actualizadatos(int codigo)
+        {
+            codigo = 1030522;
+
+            var modelo4 = from m in entidad.Compras
+                          where m.Cod_empresa == codigo
+                          select m;
+
+            return View(modelo4.ToList());
+
+        }
+
+      
+
+        public ActionResult buscar() 
+        {
+
+            return View();
+        
+        }
+
+      
+
+
+
+
+
+    }
+}
---
+++ Controllers/MantenimientoController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Client_Control_001.Models;
+
+namespace Client_Control_001.Controllers
+{
+    public class MantenimientoController : Controller
+    {
+        private Data_Client_ControlEntities db = new Data_Client_ControlEntities();
+
+        //
+        // GET: /Mantenimiento/
+
+        public ActionResult Index()
+        {
+            var compras = db.Compras.Include(c => c.Empresas);
+            return View(compras.ToList());
+        }
+
+        //
+        // GET: /Mantenimiento/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Compras compras = db.Compras.Find(id);
+            if (compras == null)
+            {
+                return HttpNotFound();
+            }
+            return View(compras);
+        }
+
+        //
+        // GET: /Mantenimiento/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.Cod_empresa = new SelectList(db.Empresas, "Cod_empresa", "Cod_empresa");
+            return View();
+        }
+
+        //
+        // POST: /Mantenimiento/Create
+
+        [HttpPost]
+        public ActionResult Create(Compras compras)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Compras.Add(compras);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Cod_empresa = new SelectList(db.Empresas, "Cod_empresa", "Cod_empresa", compras.Cod_empresa);
+            return View(compras);
+        }
+
+        //
+        // GET: /Mantenimiento/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Compras compras = db.Compras.Find(id);
+            if (compras == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Cod_empresa = new SelectList(db.Empresas, "Cod_empresa", "Cod_empresa", compras.Cod_empresa);
+            return View(compras);
+        }
+
+        //
+        // POST: /Mantenimiento/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Compras compras)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(compras).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Cod_empresa = new SelectList(db.Empresas, "Cod_empresa", "Cod_empresa", compras.Cod_empresa);
+            return View(compras);
+        }
+
+        //
+        // GET: /Mantenimiento/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Compras compras = db.Compras.Find(id);
+            if (compras == null)
+            {
+                return HttpNotFound();
+            }
+            return View(compras);
+        }
+
+        //
+        // POST: /Mantenimiento/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Compras compras = db.Compras.Find(id);
+            db.Compras.Remove(compras);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
